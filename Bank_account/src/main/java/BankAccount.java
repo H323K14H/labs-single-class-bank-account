91cad279ce1c942f38731bff5624d3a2d4ff8b9c
Hezekiah Adeusi <hezekiaha58@gmail.com>
2023-10-12 00:42:23 +0100

    Added properties to BankAccount Class. Added constructor to BankAccount Class. Added getters and setters for each property in BankAccount Class. Added Tests in BankAccountTest.java for each getter and setter in BankAcount Class.

--- Bank_account/src/main/java/BankAccount.java
+++ Bank_account/src/main/java/BankAccount.java
@@ -1,2 +1,71 @@
+import java.time.LocalDate;
+
 public class BankAccount {
+    private String firstName;
+    private String lastName;
+    private LocalDate dateOfBirth;
+    private int accountNumber;
+    private int bankBalance;
+
+    public BankAccount(String firstName, String lastName, LocalDate dateOfBirth, int accountNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.accountNumber = accountNumber;
+        this.bankBalance = 0;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String setFirstName) {
+        this.firstName = setFirstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String setLastName) {
+        this.lastName = setLastName;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return this.dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate setDateOfBirth) {
+        this.dateOfBirth = setDateOfBirth;
+    }
+
+    public int getAccountNumber() {
+        return this.accountNumber;
+    }
+
+    public void setAccountNumber(int setAccountNumber) {
+        this.accountNumber = setAccountNumber;
+    }
+
+    public int getBankBalance() {
+        return this.bankBalance;
+    }
+
+    public void setBankBalance(int setBankBalance) {
+        this.bankBalance = setBankBalance;
+    }
+
+    public int deposit(int amount) {
+        return this.bankBalance + amount;
+    }
+
+    public int withdraw(int amount) {
+        return this.bankBalance - amount;
+    }
+
+    public int payInterest() {
+        return (int) (
+                this.bankBalance + (this.bankBalance * 0.05)
+        );
+    }
 }
--- Bank_account/src/test/java/BankAccountTest.java
+++ Bank_account/src/test/java/BankAccountTest.java
@@ -1,2 +1,186 @@
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class BankAccountTest {
+
+    @Test
+    void canGetFirstName() {
+        BankAccount b1 = new BankAccount(
+                "Bob",
+                "Bobby",
+                LocalDate.of(2003, 10, 8),
+                11111111
+        );
+        String fName = b1.getFirstName();
+
+        assertThat(fName).isEqualTo("Bob");
+    }
+
+    @Test
+    void canSetFirstName() {
+        BankAccount b1 = new BankAccount(
+                "Bob",
+                "Bobby",
+                LocalDate.of(2003, 10, 8),
+                11111111
+        );
+        b1.setFirstName("Darcy");
+        String fName = b1.getFirstName();
+
+        assertThat(fName).isEqualTo("Darcy");
+
+    }
+
+    //
+    @Test
+    void canGetLastName() {
+        BankAccount b1 = new BankAccount(
+                "Bob",
+                "Bobby",
+                LocalDate.of(2003, 10, 8),
+                11111111
+        );
+        String lName = b1.getLastName();
+
+        assertThat(lName).isEqualTo("Bobby");
+    }
+
+    @Test
+    void canSetLastName() {
+        BankAccount b1 = new BankAccount(
+                "Bob",
+                "Bobby",
+                LocalDate.of(2003, 10, 8),
+                11111111
+        );
+        b1.setLastName("Fillet");
+        String lName = b1.getLastName();
+
+        assertThat(lName).isEqualTo("Fillet");
+    }
+
+    //
+    @Test
+    void canGetDateOfBirth() {
+        BankAccount b1 = new BankAccount(
+                "Bob",
+                "Bobby",
+                LocalDate.of(2003, 10, 8),
+                11111111
+        );
+        LocalDate dob = b1.getDateOfBirth();
+
+        assertThat(dob).isEqualTo(LocalDate.of(2003, 10, 8));
+    }
+
+    @Test
+    void canSetDateOfBirth() {
+        BankAccount b1 = new BankAccount(
+                "Bob",
+                "Bobby",
+                LocalDate.of(2003, 10, 8),
+                11111111
+        );
+        b1.setDateOfBirth(LocalDate.of(2020, 2, 29));
+        LocalDate dob = b1.getDateOfBirth();
+
+        assertThat(dob).isEqualTo(LocalDate.of(2020, 2, 29));
+    }
+
+    @Test
+    void canGetAccountNumber() {
+        BankAccount b1 = new BankAccount(
+                "Bob",
+                "Bobby",
+                LocalDate.of(2003, 10, 8),
+                11111111
+        );
+        int accountNumber = b1.getAccountNumber();
+        assertThat(accountNumber).isEqualTo(11111111);
+    }
+
+    @Test
+    void canSetAccountNumber() {
+        BankAccount b1 = new BankAccount(
+                "Bob",
+                "Bobby",
+                LocalDate.of(2003, 10, 8),
+                11111111
+        );
+        b1.setAccountNumber(22222222);
+        int accountNumber = b1.getAccountNumber();
+        assertThat(accountNumber).isEqualTo(22222222);
+    }
+
+    @Test
+    void canGetBankBalance() {
+        BankAccount b1 = new BankAccount(
+                "Bob",
+                "Bobby",
+                LocalDate.of(2003, 10, 8),
+                11111111
+        );
+        int bankBalance = b1.getBankBalance();
+        assertThat(bankBalance).isEqualTo(0);
+    }
+
+    @Test
+    void canSetBankBalance() {
+        BankAccount b1 = new BankAccount(
+                "Bob",
+                "Bobby",
+                LocalDate.of(2003, 10, 8),
+                11111111
+        );
+        b1.setBankBalance(1000);
+        int bankBalance = b1.getBankBalance();
+        assertThat(bankBalance).isEqualTo(1000);
+    }
+
+    @Test
+    void doesDepositWork() {
+        BankAccount b1 = new BankAccount(
+                "Bob",
+                "Bobby",
+                LocalDate.of(2003, 10, 8),
+                11111111
+        );
+        b1.setBankBalance(b1.deposit(50));
+        int updatedBalance = b1.getBankBalance();
+        assertThat(updatedBalance).isEqualTo(50);
+
+    }
+
+    @Test
+    void doesWithdrawWork() {
+        BankAccount b1 = new BankAccount(
+                "Bob",
+                "Bobby",
+                LocalDate.of(2003, 10, 8),
+                11111111
+        );
+        b1.setBankBalance(b1.deposit(100));
+        b1.setBankBalance(b1.withdraw(20));
+        int updatedBalance = b1.getBankBalance();
+
+        assertThat(updatedBalance).isEqualTo(80);
+    }
+
+    @Test
+    void doesInterestPay() {
+        BankAccount b1 = new BankAccount(
+                "Bob",
+                "Bobby",
+                LocalDate.of(2003, 10, 8),
+                11111111
+        );
+        b1.setBankBalance(b1.deposit(100));
+        b1.setBankBalance(b1.payInterest());
+        int updatedBalance = b1.getBankBalance();
+
+        assertThat(updatedBalance).isEqualTo(105);
+    }
 }
